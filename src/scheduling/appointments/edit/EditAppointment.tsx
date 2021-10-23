525964b583cb45e519a4ddcb96b4083edb1bef31
Chioma Nkem-Eze <fegaeze@gmail.com>
2021-10-23 15:04:41 +0300

    Test one pass - (edit appointment)

--- src/scheduling/appointments/edit/EditAppointment.tsx
+++ src/scheduling/appointments/edit/EditAppointment.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import usePatient from '../../../patients/hooks/usePatient'
+import useAppointment from '../../hooks/useAppointment'
+
 
 const EditAppointment = () => {
+  const { id } = useParams()
+
+  const { data: currentAppointment, isLoading: isLoadingAppointment } = useAppointment(id)
+  const { data: patient } = usePatient(currentAppointment ? currentAppointment.patient : id)
+  
+
   return (
     <div>
 
